139c21afb8a4c666884f5ac8adf02d0b9fb13acf
Akku121abi <bidlaji580@gmail.com>
2026-08-04 16:19:27 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Firebase App
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwU2YXNbJqBc2s9ck2CoNMFupXLf7DJPs",
+  authDomain: "myshop-508c1.firebaseapp.com",
+  projectId: "myshop-508c1",
+  storageBucket: "myshop-508c1.firebasestorage.app",
+  messagingSenderId: "357256395224",
+  appId: "1:357256395224:web:d2c6e4523e8ab5540a9aad"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
