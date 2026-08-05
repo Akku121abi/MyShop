77794ebc33cb3bf5eb174626372f0d2391bcf1dc
Akku121abi <bidlaji580@gmail.com>
2026-08-05 15:17:38 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,59 +1,54 @@
 import { db } from "./firebase.js";
-
 import {
   collection,
   addDoc,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// ===== Total Amount =====
+// अभी टेस्ट के लिए 499 रखा है।
+let totalAmount = Number(localStorage.getItem("cartTotal")) || 499;
+
+// Show Amount
+document.getElementById("amountText").innerHTML =
+`Total Amount : ₹${totalAmount}`;
+
+// Unique Order ID
+const orderId = "ORD" + Date.now();
+
+// UPI Link
+const upiLink =
+`upi://pay?pa=skyler4567@ybl&pn=Akash&am=${totalAmount}&cu=INR&tn=${orderId}`;
+
+// QR Generate
+new QRCode(document.getElementById("qrcode"), {
+    text: upiLink,
+    width: 250,
+    height: 250
+});
+
+// Pay Button
+document.getElementById("payBtn").href = upiLink;
+
+// Button
 const btn = document.getElementById("placeOrderBtn");
 
 btn.addEventListener("click", async () => {
 
-  const name = document.getElementById("name").value.trim();
-  const mobile = document.getElementById("mobile").value.trim();
-  const address = document.getElementById("address").value.trim();
-  const screenshot = document.getElementById("screenshot").files[0];
+    const name = document.getElementById("name").value.trim();
+    const mobile = document.getElementById("mobile").value.trim();
+    const address = document.getElementById("address").value.trim();
+    const screenshot = document.getElementById("screenshot").files[0];
 
-  if (!name || !mobile || !address) {
-    alert("Please fill all details.");
-    return;
-  }
+    if (!name || !mobile || !address) {
+        alert("Please fill all details.");
+        return;
+    }
 
-  if (!screenshot) {
-    alert("Please upload payment screenshot.");
-    return;
-  }
+    if (!screenshot) {
+        alert("Please upload payment screenshot.");
+        return;
+    }
 
-  try {
-
-    await addDoc(collection(db, "orders"), {
-
-      customerName: name,
-      mobile: mobile,
-      address: address,
-
-      paymentStatus: "Pending Verification",
-
-      screenshotName: screenshot.name,
-
-      createdAt: serverTimestamp()
-
-    });
-
-    alert("✅ Order Placed Successfully!\nPayment verification ke baad order process hoga.");
-
-    document.getElementById("name").value = "";
-    document.getElementById("mobile").value = "";
-    document.getElementById("address").value = "";
-    document.getElementById("screenshot").value = "";
-
-  } catch (e) {
-
-    console.error(e);
-
-    alert("❌ " + e.message);
-
-  }
-
+    alert("Part 1 Working ✅");
 });
